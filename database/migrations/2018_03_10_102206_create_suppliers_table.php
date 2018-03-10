0cb81dc18ae8b35cc53c65063e46d5fafc2edf80
adyopo <aoporanu@gmail.com>
2018-03-10 12:26:47 +0200

    Initial commit of exciting project

---
+++ database/migrations/2018_03_10_102206_create_suppliers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('stock_id')->unsigned()->index()->nullable();
+            $table->foreign('stock_id')->references('id')->on('stocks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
